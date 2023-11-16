cee8abd1bb4c08129a1dd953c1cf827dd93aa70b
Marcio Sousa <marciofrapeso@gmail.com>
2023-11-16 13:17:03 -0300

    Seção 3: NET 6 - Modelo de Domínio - Entity Framework Core.

---
+++ LanchesMac/Migrations/20231116003838_PopularLanches.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class PopularLanches : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Lanches (CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemUrl, IsLanchePreferido, Nome, Preco) VALUES (1, 'Pão, hamburguer, ovo, presunto, queijo e batata palha','Delicioso pão de hamburguer com ovo frito; presunto e queijo de primeira qualidade acompanhado com batata palha', 1,'http://www.macoratti.net/Imagens/lanches/cheesesalada1.jpg','http://www.macoratti.net/Imagens/lanches/cheesesalada1.jpg',0, 'Cheese Salada', 12.50)");
+            migrationBuilder.Sql("INSERT INTO Lanches (CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemUrl, IsLanchePreferido, Nome, Preco) VALUES (1, 'Pão, presunto, mussarela e tomate','Um dos melhores lanches já vistos',                                                                                                   1,'http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg','http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg',0, 'Misto Quente', 8.00)");
+            migrationBuilder.Sql("INSERT INTO Lanches (CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemUrl, IsLanchePreferido, Nome, Preco) VALUES (1, 'Pão, hamburguer, presunto, mussarela e batata palha','Excelente lanche para quando se está sozinho',                                                                      1,'http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg','http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg',0, 'Cheese Burger', 11.00)");
+            migrationBuilder.Sql("INSERT INTO Lanches (CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemUrl, IsLanchePreferido, Nome, Preco) VALUES (2, 'Pão, integral, queijo branco, peito de peru, cenoura, alface, iogurte natural','Uma delícia de lanche para acompanhar com bebida',                                        1,'http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg','http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg',1, 'Lanche Natural Peito de Peru', 15.00)");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Lanches");
+        }
+    }
+}
---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage ="Informe o nome da categoria.")]
+        [StringLength(100, ErrorMessage ="O tamanho máximo é 100 Caracteres.")]
+        [Display(Name ="Nome")]
+        public string CategoriaNome { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição da categoria.")]
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 Caracteres.")]
+        [Display(Name = "Descriçao")]
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
+    }
+}
